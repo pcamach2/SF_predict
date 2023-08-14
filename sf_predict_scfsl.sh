8b53fc23773a3d8d85eacf4d95f6c74ecb7dd677
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2023-08-14 08:32:13 -0500

    Update sf_predict_scfsl.sh

--- sf_predict_scfsl.sh
+++ sf_predict_scfsl.sh
@@ -21,23 +21,23 @@ prediction_dir=$4
 NOW=$(date "+%D-%T")
 if [ "${save}" == "yes" ];
 then
-	singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_diag1_percent_save_v3.py
+	singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_percent_save_v5.py dti_files fcon_files no_scramble
 	exit 0
 
 elif [ "${save}" == "no" ];
 then
 	if [ "${SLURM_ARRAY_TASK_ID}" == "1" ];
 	then
-	    singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_diag1_percent_part1.py
+	    singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_percent_part1.py no_scramble
     elif [ "${SLURM_ARRAY_TASK_ID}" == "2" ];
 	then
-		singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_diag1_percent_part2.py
+		singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_percent_part2.py no_scramble
     elif [ "${SLURM_ARRAY_TASK_ID}" == "3" ];	
 	then	
-		singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_diag1_percent_part3.py
+		singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_percent_part3.py no_scramble
     elif [ "${SLURM_ARRAY_TASK_ID}" == "4" ];
 	then
-		singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_diag1_percent_part4.py
+		singularity exec -B ${base_dir}/${version}/testing/${prediction_dir}:/datain ${base_dir}/singularity_images/pyconnpredict_0.2.6.sif python3 /datain/test_sf_prediction_riemannian_scfsl_percent_part4.py no_scramble
 	fi
         exit 0
 fi
